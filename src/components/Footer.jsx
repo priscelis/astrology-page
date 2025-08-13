57cf5d275bc5c34c369f6ca8014f698c6538d552
priscelis <prisceliscodrington@gmail.com>
2025-08-13 16:14:31 +0200

    Adding a footer to the web

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <>
+    <footer className="text-white flex justify-center text-center text-xl">
+    <p>© 2025  Proyecto realizado por Priscelis Codrington para <br/>el Bootcamp Fullstack (Femcoders – Factoría F5).</p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer

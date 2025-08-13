58a7ed4db3d33233a3e73e933e7b4f43d141437d
priscelis <prisceliscodrington@gmail.com>
2025-08-13 16:11:40 +0200

    Background adjustment

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
 
 
 const Layout = () => (
   <>
-  
+   <main className="w-full h-screen  rounded-xl  bg-gradient-to-b from-[#D6AC71]/70 to-[#251B13]/30">
     <Outlet />
 
+    
+  </main>
+<Footer/>
   </>
 );
 

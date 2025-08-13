fdadd6d30355b2f19d30ea250e1c9dbaba1e76ee
priscelis <prisceliscodrington@gmail.com>
2025-08-13 16:12:50 +0200

    Removing everything

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-    <div className="w-full h-screen  rounded-xl  bg-gradient-to-b from-[#D6AC71]/70 to-[#251B13]/30">
+   
 
     
     
-    </div>
+  
+
+   
+   
     </>
   )
 }
